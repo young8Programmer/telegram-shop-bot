539b085d6ac78fd1b0e3a34cd9e4a0cbe026fb34
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:21 +0500

    style: kod uslubini yaxshilash

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// API hujjatlarini qo'shish
 import { Cart } from './cart.entity';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';

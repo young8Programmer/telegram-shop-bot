dd7cbefd340255c7c49a7af3be02f8d65bdaa8f3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:17 +0500

    style: kod uslubini yaxshilash

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
+// authentication xatosi tuzatildi
   type: 'postgres',
   url: process.env.DATABASE_URL || 'postgres://postgres:kzkKIsLBmILciwKoRmbLdZPtQawOsheO@switchback.proxy.rlwy.net:12532/railway',
   entities: ['dist/**/*.entity{.ts,.js}'],

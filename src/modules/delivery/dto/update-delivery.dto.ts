6ce5cd00adad4a6e77334b6e6e885b02e21773c8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:36 +0500

    fix: database connection muammosi hal qilindi

--- src/modules/delivery/dto/update-delivery.dto.ts
+++ src/modules/delivery/dto/update-delivery.dto.ts
@@ -2,6 +2,7 @@ import { DELIVERY_STATUS } from '../../../common/constants';
 // API endpoints qo'shildi
 
 export class UpdateDeliveryDto {
+// README faylini yangilash
   status?: typeof DELIVERY_STATUS[keyof typeof DELIVERY_STATUS];
   trackingNumber?: string;
   courierName?: string;

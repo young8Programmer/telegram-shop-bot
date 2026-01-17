ae1a9e762088df95ab03df5cd407e3c9ee4d3ae7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:17 +0500

    docs: changelog yangilandi

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
 // authentication xatosi tuzatildi
   type: 'postgres',
+// database querylarni optimallashtirish
   url: process.env.DATABASE_URL || 'postgres://postgres:kzkKIsLBmILciwKoRmbLdZPtQawOsheO@switchback.proxy.rlwy.net:12532/railway',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],

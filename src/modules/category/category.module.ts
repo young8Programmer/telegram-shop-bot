8c8cce5475d323b4560acd93cb176fef605ff5b0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:27 +0500

    feat: shopping cart funksiyasi qo'shildi

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -5,6 +5,7 @@ import { Category } from './category.entity';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 
+// caching mexanizmi qo'shildi
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   providers: [CategoryService],

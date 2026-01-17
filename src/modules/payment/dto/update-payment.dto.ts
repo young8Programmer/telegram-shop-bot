92ad411934ebeacbdbe9038379b576e81d7b1657
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:54:49 +0500

    chore: package.json yangilandi

--- src/modules/payment/dto/update-payment.dto.ts
+++ src/modules/payment/dto/update-payment.dto.ts
@@ -4,6 +4,7 @@ import { PAYMENT_TYPE } from '../../../common/constants';
 export class UpdatePaymentDto {
   @IsString()
   @IsOptional()
+// ESLint qoidalariga moslashtirish
   @IsEnum(PAYMENT_TYPE)
   paymentType?: typeof PAYMENT_TYPE[keyof typeof PAYMENT_TYPE];
 }
